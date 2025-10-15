6e8899fc376aaf8bfcf5fee1eac65a3ce4cad566
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2025-10-15 17:20:41 -0600

    Basic Player/Bird controls
    Added a bird prefab that will be used for the player and machine learning agent. (I'm not sure how the ML agent works so lmk). Made it fall and jump on a spacebar input. Also I'm using the base input map

---
+++ Assets/Objects/Birds/BirdBase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdBase : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public BirdStats stats;
+    float jumpStrength;
+    PlayerInput playerInput;
+    Rigidbody2D rigidBody;
+    
+    void Start()
+    {
+        jumpStrength = stats.jumpStrength;
+        playerInput = GetComponent<PlayerInput>();
+        rigidBody =  GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnJump()
+    {
+        //zero out the velocity so that its more cartoony (feel free to fuck with this)
+        rigidBody.linearVelocityY = 0;
+        
+        rigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+    }
+    
+}
---
+++ Assets/Objects/Birds/BirdStats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BirdStats", menuName = "Scriptable Objects/BirdStats")]
+public class BirdStats : ScriptableObject
+{
+    public float jumpStrength;
+}
