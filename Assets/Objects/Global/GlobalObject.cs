32d2310f00edf758cbda7d6851b7d83fae32569c
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2025-10-18 21:11:06 -0600

    Implemented Score
    Added a score property in the bird base class, also I added an event that notifies when the score is changed for ui stuff
    Added a scorebox object.

---
+++ Assets/Objects/Global/GlobalObject.cs
@@ -0,0 +1,19 @@
+using HelperScripts;
+using UnityEngine;
+
+public class GlobalObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
